53c34e8e2a282d74f35008d6355bf1ccd176edd4
guihvicentini <guilherme.h.vicentini@hotmail.com>
2023-12-28 23:15:47 +0100

    fix formula style

--- Formula/quickgen.rb
+++ Formula/quickgen.rb
@@ -1,5 +1,5 @@
 class Quickgen < Formula
-  desc "Rust implementation of a random generator for different random numbers like uuid or passwords"
+  desc "Rust implementation of a uuid generator"
   homepage "https://github.com/GuihVicentini/quickgen"
   url "https://github.com/GuihVicentini/quickgen/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "b8eb57a0d523eaff2f6b3e011e3807cdd1b314829afdc0b222e54dfd8c13bb58"
